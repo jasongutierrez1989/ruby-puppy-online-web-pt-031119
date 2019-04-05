961be60b7532f0e8c686da3e2312a302b1623289
JASON GUTIERREZ <jasongutierrez1989@gmail.com>
2019-04-05 03:42:37 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,19 +1,21 @@
 class Dog
-  attr_accessor :name
+
   @@all = []
+  
+  attr_accessor :name  
 
   def initialize(name)
     @name = name
-    @@all.push(name)
+    @@all << self
   end
 
-    def self.all
-      @@all.each do |name|
-        puts name
-      end
+  def self.all
+    @@all.each do |name|
+      puts name
     end
+  end
 
-    def self.clear_all
-      @@all.clear
-    end
+  def self.clear_all
+    @@all.clear
+  end
 end
